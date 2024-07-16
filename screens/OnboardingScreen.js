cfaa95979322539176727999789d5f854aae823b
DangToVu <164459776+DangToVu@users.noreply.github.com>
2024-07-16 15:47:43 +0700

    Update OnboardingScreen.js

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,39 +1,70 @@
 // screens/OnboardingScreen.js
 import React from 'react';
-import { View, Text, Button, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, ImageBackground, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function OnboardingScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Image source={require('../assets/1st.png')} style={styles.image} />
-      <Text style={styles.title}>Welcome to our store</Text>
-      <Text style={styles.subtitle}>Get your groceries in as fast as one hour</Text>
-      <Button title="Get Started" onPress={() => navigation.navigate('SignIn')} />
-    </View>
+    <ImageBackground source={require('../assets/1st.png')} style={styles.backgroundImage}>
+      <View style={styles.container}>
+        <Image source={require('../assets/Group.png')} style={styles.carrotImage} />
+        <Text style={styles.title}>Welcome </Text>
+        <Text style={styles.title2}> to our store</Text>
+        <Text style={styles.subtitle}>Get your groceries in as fast as one hour</Text>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('SignIn')}>
+          <Text style={styles.buttonText}>Get Started</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  backgroundImage: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#fff',
   },
-  image: {
+  container: {
     width: '100%',
-    height: '50%',
-    resizeMode: 'cover',
+    padding: 20,
+    alignItems: 'center',
+  },
+  carrotImage: {
+    width: '10%',
+    height: '10%',
+    marginBottom: 10,
+    alignItems: 'center',
   },
   title: {
-    fontSize: 24,
+    fontSize: 40,
     fontWeight: 'bold',
-    marginTop: 20,
+    color: '#fff',
+    textAlign: 'center',
+    marginBottom: 0,
+  },
+  title2: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+    marginBottom: 10,
   },
   subtitle: {
     fontSize: 16,
-    color: '#777',
+    color: '#ddd',
     textAlign: 'center',
-    marginVertical: 20,
+    marginBottom: 40,
+  },
+  button: {
+    backgroundColor: '#34c759',
+    paddingVertical: 22,
+    paddingHorizontal: 115,
+    borderRadius: 18,
+    marginBottom: 50
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
